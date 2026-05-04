eabd2673f3233081cdce0d76f149f0e57b80f818
mirenja <gillian.kenyani@code.berlin>
2026-05-04 11:45:32 +0200

    streamlite link

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,7 +1,22 @@
 import pandas as pd
+import gdown
+from pathlib import Path
+import os
 
-def load_and_prepare(path='data/borg_traces_data.csv'):
-    df = pd.read_csv(path)
+DATA_PATH = Path("data/borg_traces_data.csv")
+FILE_ID   = "1jhOhl4CcGbnkocOFHw9EYownBRSUQtV6"
+
+
+
+def load_and_prepare():
+    if not DATA_PATH.exists():
+        DATA_PATH.parent.mkdir(exist_ok=True)
+        gdown.download(
+            f"https://drive.google.com/uc?id={FILE_ID}",
+            str(DATA_PATH),
+            quiet=False
+        )
+    df = pd.read_csv(DATA_PATH)
     df['time_sec'] = df['time'] / 1_000_000
     df['label_raw'] = ((df['event'] == 'FAIL') | (df['failed'] == 1)).astype(int)
     df = df.sort_values(['machine_id', 'time'])
